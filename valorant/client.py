db07c1c7bdc082fd4b7db56686c8c1f0e7d84c12
Name?= null <Ire.Raufu76@gmail.com>
2020-11-11 20:39:44 +0000

    Updated code style: Black

--- valorant/client.py
+++ valorant/client.py
@@ -2,12 +2,12 @@ import json
 import locale
 import requests
 
-#from .errors import APIError
+# from .errors import APIError
 from .errors import InvalidKeyError
 
 LOCALE = locale.getlocale()[0].replace("_", "-")
 CONTENT_URL = "https://na.api.riotgames.com/val/content/v1/contents"
-headerMixin = { "Accept-Charset" : "application/x-www-form-urlencoded; charset=UTF-8"}
+headerMixin = {"Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8"}
 
 
 def update_dict(stale, latest):
@@ -18,30 +18,31 @@ def update_dict(stale, latest):
 
 
 class Client(object):
-	def __init__(self, key, locale=LOCALE):
-		if not self.verify_key(key):
-			raise InvalidKeyError(
-				f"Your provided API Key: {key} is invalid or expired."
-			)
-		else:
-			self.key = key
-			self.locale = locale
-			headers = update_dict(headerMixin, {"X-Riot-Token" : self.key})
-			r = requests.get(CONTENT_URL, headers=headers).json()
+    def __init__(self, key, locale=LOCALE):
+        if not self.verify_key(key):
+            raise InvalidKeyError(
+                f"Your provided API Key: {key} is invalid or expired."
+            )
+        else:
+            self.key = key
+            self.locale = locale
+            headers = update_dict(headerMixin, {"X-Riot-Token": self.key})
+            r = requests.get(CONTENT_URL, headers=headers).json()
 
-			self.set_attributes(r)
+            self.set_attributes(r)
 
-	def __getattribute__(self, name):
-		return super(Client, self).__getattribute__(name)
+    def __getattribute__(self, name):
+        return super(Client, self).__getattribute__(name)
 
-	def set_attributes(self, attrs):
-		for attr, value in attrs.items():
-			self.__setattr__(attr, value)
-	
-	def verify_key(self, key):
-		headers = update_dict(headerMixin, {"X-Riot-Token" : key})
-		r = requests.get(CONTENT_URL, headers=headers)
+    def set_attributes(self, attrs):
+        for attr, value in attrs.items():
+            self.__setattr__(attr, value)
 
-		if str(r.status_code)[0] != "2": return False
-		else: return True
-	
+    def verify_key(self, key):
+        headers = update_dict(headerMixin, {"X-Riot-Token": key})
+        r = requests.get(CONTENT_URL, headers=headers)
+
+        if str(r.status_code)[0] != "2":
+            return False
+        else:
+            return True
--- valorant/errors.py
+++ valorant/errors.py
@@ -1,5 +1,6 @@
 class APIError(Exception):
     pass
 
+
 class InvalidKeyError(Exception):
-	pass
+    pass
