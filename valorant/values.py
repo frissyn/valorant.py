5a771ee0171b10fd1e2fd91a9463d2e2c99d7772
Name?= null <Ire.Raufu76@gmail.com>
2021-01-05 14:25:57 +0000

    add request customization

--- valorant/values.py
+++ valorant/values.py
@@ -13,32 +13,12 @@ ENDPOINTS = {
     "content": "val/content/v1/contents",
     "status": "val/status/v1/platform-data",
     "puuid": "riot/account/v1/accounts/by-puuid/{puuid}",
-    "gamename": "riot/account/v1/accounts/by-riot-id/{name}/{tag}"
+    "gamename": "riot/account/v1/accounts/by-riot-id/{name}/{tag}",
+    "match": "val/match/v1/matches/{matchID}",
+    "match-history": "val/match/v1/matchlists/by-puuid/{puuid}",
+    "match-queue": "val/match/v1/recent-matches/by-queue/{queue}"
 }
 
 HEADERS = {
     "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8"
 }
-
-def _build_header(mixin: dict):
-    c = HEADERS.copy()
-
-    for n, v in mixin.items():
-        c[n] = v
-    
-    return c
-
-def _build_url(code="na", endpoint="content", p=None):
-    if code not in REIGONS and code not in ROUTES:
-        raise ValueError(f"Invalid Route Code: '{code}'")
-    else:
-        pass
-
-    try:
-        end = ENDPOINTS[endpoint]
-    except ValueError:
-        raise ValueError(f"The endpoint '{endpoint}' does not exist.")
-
-    url = BASE_URL.format(code=code) + end
-
-    return url
