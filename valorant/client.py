8189fcf0c5cd7972e71e0ca1a8e311d697249ba2
Ire <62220201+frissyn@users.noreply.github.com>
2021-12-08 17:16:49 +0000

    🔧: redesign content caching, add typing to Client

--- valorant/client.py
+++ valorant/client.py
@@ -1,3 +1,4 @@
+import typing as t
 import urllib.parse
 
 from .lexicon import Lex
@@ -6,50 +7,46 @@ from .caller import WebCaller
 
 from .objects import ActDTO
 from .objects import AccountDTO
+from .objects import ContentDTO
 from .objects import ContentList
 from .objects import ContentItemDTO
 from .objects import LeaderboardDTO
 from .objects import PlatformDataDTO
 
 
-def update(stale: dict, latest: dict) -> dict:
-    for key, items in latest.items():
-        stale[key] = latest[key]
-
-    return stale
-
-
 class Client(object):
-    def __init__(self, key, locale=Lex.LOCALE, region="na", route="americas", reload=True):
+    def __init__(
+        self,
+        key: t.Text,
+        locale: t.Optional[t.Text] = Lex.LOCALE, 
+        region: t.Text = "na",
+        route: t.Text ="americas",
+        load: bool = True
+    ):
         self.key = key
         self.route = route
         self.locale = locale
         self.region = region
         self.handle = WebCaller(key, locale, region, route)
 
-        if reload:
-            self.reload()
-        else:
-            pass
+        if load:
+            self.get_content(cache=True)
+    
+    def _content_if_cache(self):
+        return getattr(self, "content", self.get_content())
 
     def __getattribute__(self, name):
         return super(Client, self).__getattribute__(name)
 
-    def set_attributes(self, attrs) -> None:
-        for attr, value in attrs.items():
-            self.__setattr__(attr, value)
+    def get_content(self, cache: bool = True) -> ContentDTO:
+        content = ContentDTO(self.handle.call("GET", "content"))
 
-        return
-
-    def reload(self) -> None:
-        """Reload the current cached response for the VAL-CONTENT endpoints."""
-        r = self.handle.call("GET", "content")
-        self.set_attributes(r)
-
-        return
+        if cache:
+            self.content = content
+        
+        return content
     
-    def asset(self, **kw):
-        """Get any Valorant content asset by it's UUID."""
+    def asset(self, **kw: t.Mapping):
         if len(kw) > 1: raise ValueError
 
         key = str(list(kw.keys())[0])
@@ -61,7 +58,7 @@ class Client(object):
             genexpr = lambda m: m[key] == value
         
         for name in Lex.CONTENT_NAMES:
-            for asset in getattr(self, name):
+            for asset in getattr(self.content, name):
                 try:
                     if genexpr(asset):
                         return ContentItemDTO(asset)
@@ -70,13 +67,13 @@ class Client(object):
     
         return None
 
-    def get_user_by_puuid(self, puuid: str) -> AccountDTO:
+    def get_user_by_puuid(self, puuid: t.Text) -> AccountDTO:
         """Get a Riot AccountDTO by the given PUUID."""
         r = self.handle.call("GET", "puuid", puuid=puuid)
 
         return AccountDTO(r, self.handle)
 
-    def get_user_by_name(self, name: str) -> AccountDTO:
+    def get_user_by_name(self, name: t.Text) -> AccountDTO:
         """Get a Riot AccountDTO by a given name and tag."""
         vals = name.split("#")
         vals = [urllib.parse.quote(v, safe=Lex.SAFES) for v in vals]
@@ -90,47 +87,51 @@ class Client(object):
 
         return PlatformDataDTO(r)
 
-    def get_acts(self) -> ContentList:
+    def get_acts(self) -> t.Iterable[ActDTO]:
         """Get a ContentList of Acts from Valorant."""
-        acts = [ActDTO(a) for a in self.acts]
+        content = self._content_if_cache()
+        acts = [ActDTO(a) for a in content.acts]
 
         return ContentList(acts)
 
-    def get_characters(self) -> ContentList:
+    def get_characters(self) -> t.Iterable[ContentItemDTO]:
         """Get a ContentList of Agents from Valorant."""
-        characters = [ContentItemDTO(c) for c in self.characters]
+        content = self._content_if_cache()
+        characters = [ContentItemDTO(c) for c in content.characters]
 
         return ContentList(characters)
 
-    def get_current_act(self) -> ActDTO:
+    def get_current_act(self) -> t.Optional[ActDTO]:
         """Get the current Act (indiscriminate of episode)."""
         for act in self.get_acts():
-            if act.isActive and "ACT" in act.name:
+            if act.isActive:
                 return act
-            else:
-                continue
 
         return None
 
     def get_charms(self) -> ContentList:
         """Get a ContentList of Gun Buddies from Valorant."""
-        charms = [ContentItemDTO(c) for c in self.charms]
+        content = self._content_if_cache()
+        charms = [ContentItemDTO(c) for c in content.charms]
 
         return ContentList(charms)
 
     def get_charm_levels(self) -> ContentList:
         """Get a ContentList of Gun Buddy Levels from Valorant."""
-        charmLevels = [ContentItemDTO(c) for c in self.charmLevels]
+        content = self._content_if_cache()
+        charmLevels = [ContentItemDTO(c) for c in content.charmLevels]
 
         return ContentList(charmLevels)
 
     def get_chromas(self) -> ContentList:
-        chromas = [ContentItemDTO(c) for c in self.chromas]
+        content = self._content_if_cache()
+        chromas = [ContentItemDTO(c) for c in content.chromas]
 
         return ContentList(chromas)
 
     def get_equips(self) -> ContentList:
-        equips = [ContentItemDTO(e) for e in self.equips]
+        content = self._content_if_cache()
+        equips = [ContentItemDTO(e) for e in content.equips]
 
         return ContentList(equips)
 
@@ -145,48 +146,56 @@ class Client(object):
 
     def get_maps(self) -> ContentList:
         """Get a ContentList of Maps from Valorant."""
-        maps = [ContentItemDTO(m) for m in self.maps]
+        content = self._content_if_cache()
+        maps = [ContentItemDTO(m) for m in content.maps]
 
         return ContentList(maps)
 
     def get_skins(self) -> ContentList:
         """Get a ContentList of Weapon Skins from Valorant."""
-        skins = [ContentItemDTO(s) for s in self.skins]
+        content = self._content_if_cache()
+        skins = [ContentItemDTO(s) for s in content.skins]
 
         return ContentList(skins)
 
     def get_skin_levels(self) -> ContentList:
         """Get a ContentList of Weapon Skin Levels from Valorant."""
-        skinLevels = [ContentItemDTO(s) for s in self.skinLevels]
+        content = self._content_if_cache()
+        skinLevels = [ContentItemDTO(s) for s in content.skinLevels]
 
         return ContentList(skinLevels)
 
     def get_game_modes(self) -> ContentList:
         """Get a ContentList of Game Modes from Valorant."""
-        gameModes = [ContentItemDTO(g) for g in self.gameModes]
+        content = self._content_if_cache()
+        gameModes = [ContentItemDTO(g) for g in content.gameModes]
 
         return ContentList(gameModes)
 
     def get_sprays(self) -> ContentList:
         """Get a ContentList of Sprays from Valorant."""
-        sprays = [ContentItemDTO(s) for s in self.sprays]
+        content = self._content_if_cache()
+        sprays = [ContentItemDTO(s) for s in content.sprays]
 
         return ContentList(sprays)
 
     def get_spray_levels(self) -> ContentList:
         """Get a ContentList of Spray Levels from Valorant."""
-        sprayLevels = [ContentItemDTO(s) for s in self.sprayLevels]
+        content = self._content_if_cache()
+        sprayLevels = [ContentItemDTO(s) for s in content.sprayLevels]
 
         return ContentList(sprayLevels)
 
     def get_player_cards(self) -> ContentList:
         """Get a ContentList of Player Cards from Valorant."""
-        playerCards = [ContentItemDTO(p) for p in self.playerCards]
+        content = self._content_if_cache()
+        playerCards = [ContentItemDTO(p) for p in content.playerCards]
 
         return ContentList(playerCards)
 
     def get_player_titles(self) -> ContentList:
         """Get a ContentList of Player Titles from Valorant."""
-        playerTitles = [ContentItemDTO(p) for p in self.playerTitles]
+        content = self._content_if_cache()
+        playerTitles = [ContentItemDTO(p) for p in content.playerTitles]
 
         return ContentList(playerTitles)
