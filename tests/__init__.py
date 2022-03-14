2f0e55ec6e90ee3c6c587dc8e3e3bcfa23fef1b3
frissyn <62220201+frissyn@users.noreply.github.com>
2022-03-14 14:53:41 +0000

    :wrench:: add custom assertion method

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,10 +8,16 @@ class BaseTest(unittest.TestCase):
         KEY = os.environ["VALPY-KEY"]
 
         self.access_key = KEY
-        self.client = valorant.Client(KEY)
+        self.client = valorant.Client(KEY, locale=None, load_content=False)
 
     def tearDown(self):
         self.client.handle.sess.close()
+
+    def assertHasAttr(self, obj: object, attr: str):
+        exp = hasattr(obj, attr)
+
+        if not exp:
+            self.fail(f"{obj.__class__} has no attribute '{attr}'.")
 
 
 from .client import *
